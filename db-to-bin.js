724276933eaba552e71fe9264c4a6b135d2fabe8
Kevin Yeh <kyeh@apple.com>
2020-08-03 22:37:37 -0700

    post directly to binary file

--- db-to-bin.js
+++ db-to-bin.js
@@ -1,56 +1,31 @@
+const { theirHandStringToMyHandString, theirHoldStringToInt } = require("./utils");
 var sqlite3 = require("sqlite3").verbose();
 var db = new sqlite3.Database("jacks-9-6.db");
 var fs = require("fs");
-let i = 0
+let i = 0;
 
+let solutionInts = [];
 
-
-let solutionInts = []
-
-db.each("SELECT * FROM AllHands", function(err, row) {
-    const hand = theirHandStringToMyHandString(row.hand)
-    const holdBitString = row.holdBitCodeL2R.replace(/,/g, '')
-    const holdInt = parseInt(holdBitString.padStart(8, '0'), 2 );
-    solutionInts.push(holdInt)
+db.each(
+  "SELECT * FROM AllHands",
+  function (err, row) {
+    const hand = theirHandStringToMyHandString(row.hand);
+    const holdInt = theirHoldStringToInt(row.hand);
+    solutionInts.push(holdInt);
     // console.log(hand, holdBitString, holdInt );
     // process.exit()
-    if(i % 10000 === 0) console.log(i)
-    i++
-}, () => {
+    if (i % 10000 === 0) console.log(i);
+    i++;
+  },
+  () => {
     // complete
-    console.log(solutionInts.length)
+    console.log(solutionInts.length);
     const buf = Buffer.allocUnsafe(solutionInts.length);
     solutionInts.map((int, index) => {
-        buf.writeUInt8(int, index);
-    })
-    fs.writeFileSync('jacks-9-6.txt', buf, function(err) {
-        console.log(err)
+      buf.writeUInt8(int, index);
     });
-});
-
-
-function theirHandStringToMyHandString(handString) {
-    // const handString = "c004,c305,c108,c308,c109"
-    const handArray = handString.replace(/c/g, '').split(',')
-    return handArray.map(theirCardStringToMyCardString).join('')
-}
-
-function theirCardStringToMyCardString(card) {
-    const suit = parseInt(card[0])
-    const pip = parseInt(card[1] + card[2])
-    let result = ""
-
-    if(pip === 1) result += "A"
-    else if(pip === 10) result += "T"
-    else if(pip === 11) result += "J"
-    else if(pip === 12) result += "Q"
-    else if(pip === 13) result += "K"
-    else result += pip
-
-    if(suit === 0) result += "c"
-    if(suit === 1) result += "d"
-    if(suit === 2) result += "h"
-    if(suit === 3) result += "s"
-
-    return result
-}
+    fs.writeFileSync("jacks-9-6.bin", buf, function (err) {
+      console.log(err);
+    });
+  }
+);
